044b00d1ca675dde1c7f2d9cd3718fbb0e4fd7ee
x0r <xzeror@users.noreply.github.com>
2017-02-06 21:08:04 +0300

    Initial Commit

---
+++ XZDataLayerViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZDataLayerViewer
+//
+//  Created by Andrey Ostanin on 06.02.17.
+//  Copyright © 2017 XZone Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZDataLayerViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZDataLayerViewer
+//
+//  Created by Andrey Ostanin on 06.02.17.
+//  Copyright © 2017 XZone Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
