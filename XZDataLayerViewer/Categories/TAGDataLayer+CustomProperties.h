72f85e332ef1fc91e4821a1717a376456bf3f903
x0r <xzeror@users.noreply.github.com>
2017-02-28 23:37:43 +0300

    Extracted TAGDataLayer custom properties dependency from DataLayerObserver

---
+++ XZDataLayerViewer/Categories/TAGDataLayer+CustomProperties.h
@@ -0,0 +1,14 @@
+//
+//  TAGDataLayer+Properties.h
+//  XZDataLayerViewer
+//
+//  Created by Andrey Ostanin on 28.02.17.
+//  Copyright © 2017 XZone Software. All rights reserved.
+//
+
+#import "TAGDataLayer.h"
+
+@interface TAGDataLayer (CustomProperties)
+- (NSDictionary*)dataLayerModel;
+@end
+
