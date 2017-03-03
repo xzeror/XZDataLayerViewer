1958988b4720d600073f27c4678ac0136e5aae37
x0r <xzeror@users.noreply.github.com>
2017-03-03 22:55:28 +0300

    Added ViewModel tests

---
+++ XZDataLayerViewerTests/ViewModelTest.m
@@ -0,0 +1,36 @@
+//
+//  XZDataLayerViewer - ViewModelTest.m
+//  Copyright 2017 XZone Software. All rights reserved.
+//
+//  Created by: Andrey Ostanin
+//
+
+// Class under test
+#import "ViewModel.h"
+
+// Collaborators
+
+
+@interface ViewModelTest : XCTestCase
+@end
+
+
+@implementation ViewModelTest
+
+#pragma mark - Tests
+- (void)testInitShouldSaveInjectedValues{
+	// given
+	NSString *testKey = @"testKey";
+	NSString *testValue = @"testValue";
+	BOOL testShouldShowDisclosureIndicator = YES;
+
+	// when
+	ViewModel *viewModel = [[ViewModel alloc] initWithKey:testKey value:testValue shouldShowDisclosureIndicator:testShouldShowDisclosureIndicator];
+
+	// then
+	expect(viewModel.key).to.equal(testKey);
+	expect(viewModel.value).to.equal(testValue);
+	expect(viewModel.shouldShowDisclosureIndicator).to.equal(testShouldShowDisclosureIndicator);
+}
+
+@end
